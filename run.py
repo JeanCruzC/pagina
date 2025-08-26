e6fe550f38df37771d9d17f25e1f4a994c3d1d08
Jean Marco Cruz Castellar <86123558+JeanCruzC@users.noreply.github.com>
2025-08-26 10:47:24 -0500

    Read FLASK_DEBUG env var and disable reloader

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
+import os
+
 from website import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(
+        debug=os.getenv("FLASK_DEBUG") == "1",
+        use_reloader=False,
+        host="127.0.0.1",
+        port=5000,
+    )
